e08d9286b77201860b69bb53d0f08e1e3bb803dd
Mikhail Akimov <rovinj.akimov@gmail.com>
2024-09-04 20:25:13 +0300

    tree settings

--- .config/nvim/lua/roveo/plugins/nvim-tree.lua
+++ .config/nvim/lua/roveo/plugins/nvim-tree.lua
@@ -1,13 +1,24 @@
 require("nvim-tree").setup({
 	view = {
 		width = 50,
+		signcolumn = "yes",
 	},
 	reload_on_bufenter = true,
 	sync_root_with_cwd = true,
 	respect_buf_cwd = true,
+	renderer = {
+		highlight_git = "name",
+		icons = {
+			git_placement = "signcolumn",
+		},
+	},
 	update_focused_file = {
 		enable = true,
 		update_root = true,
+	},
+	filters = {
+		git_ignored = false,
+		dotfiles = false,
 	},
 })
 
